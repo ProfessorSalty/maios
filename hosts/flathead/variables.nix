165b4eea9c4f79c971c65c929aa8f9d199b8d0fb
Greg Smith <me@gregsmith.nyc>
2025-05-11 22:43:35 -0400

    initital commit

---
+++ hosts/flathead/variables.nix
@@ -0,0 +1,41 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "Greg Smith";
+  gitEmail = "me@gregsmith.nyc";
+
+  # Hyprland Settings
+  extraMonitorSettings = "";
+
+  # Waybar Settings
+  clock24h = true;
+
+  # Program Options
+  browser = "librewolf"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+  keyboardLayout = "us";
+  consoleKeyMap = "us";
+
+  # For Nvidia Prime support
+  intelID = "PCI:1:0:0";
+  nvidiaID = "PCI:0:2:0";
+
+  # Enable NFS
+  enableNFS = true;
+
+  # Enable Printing Support
+  printEnable = false;
+
+  # Set Stylix Image
+  stylixImage = ../../wallpapers/AnimeGirlNightSky.jpg;
+
+  # Set Waybar
+  # Includes alternates such as waybar-curved.nix & waybar-ddubs.nix
+  waybarChoice = ../../modules/home/waybar/waybar-ddubs.nix;
+
+  # Set Animation style
+  # Available options are:
+  # animations-def.nix  (default)
+  # animations-end4.nix (end-4 project)
+  # animations-dynamic.nix (ml4w project)
+  animChoice = ../../modules/home/hyprland/animations-def.nix;
+}
---
+++ modules/home/default.nix
@@ -0,0 +1,35 @@
+{host, ...}: let
+  inherit (import ../../hosts/${host}/variables.nix) waybarChoice;
+in {
+  imports = [
+    ./bash.nix
+    ./bashrc-personal.nix
+    ./bat.nix
+    ./btop.nix
+    ./cava.nix
+    ./emoji.nix
+    ./fastfetch
+    ./gh.nix
+    ./ghostty.nix
+    ./git.nix
+    ./gtk.nix
+    ./htop.nix
+    ./hyprland
+    ./kitty.nix
+    ./nvf.nix
+    ./rofi
+    ./qt.nix
+    ./scripts
+    ./starship.nix
+    ./stylix.nix
+    ./swappy.nix
+    ./swaync.nix
+    ./virtmanager.nix
+    waybarChoice
+    ./wlogout
+    ./xdg.nix
+    ./yazi
+    ./zoxide.nix
+    ./zsh
+  ];
+}
